2c7a5a39af14f8242d397e5da119857fdb8e1aa1
hsteude <henrik_steude@gmx.net>
2018-11-12 14:43:15 +0100

    Added detail page scraper

--- data_scraping.py
+++ data_scraping.py
@@ -39,7 +39,7 @@ def create_links():
     return links
 
 
-def car_table_from_quote_page(quote_page):
+def car_table_from_summary_page(quote_page):
     '''
     Generates a dataframe containing all the information of the offerings
     available from the summary pages (the parsed html)
@@ -65,7 +65,7 @@ def car_table_from_quote_page(quote_page):
     details = list(map(lambda x: x.split('\n\n'), details))
     kilometers = list(map(lambda x: x[1][:-3], details))
     build = list(map(lambda x: x[2][2:], details))
-    horsepower = list(map(lambda x: x[3][9:-4], details))
+    horsepower = list(map(lambda x: x[3][-7:-4], details))#
     used = list(map(lambda x: x[4][1:], details))
     owners = list(map(lambda x: x[5][1], details))
     gear = list(map(lambda x: x[6][1:], details))
@@ -84,7 +84,7 @@ def car_table_from_quote_page(quote_page):
     ))
     return df_cars
 
-def scrape_data():
+def scrape_summary_data():
     '''
     Loops through all the links and applies the functions above.
     Outputs a data frame with the infos of all the pages (without duplicates).
@@ -94,19 +94,55 @@ def scrape_data():
     for k in range(0,len(links)):
         time.sleep(1)
         quote_page = links[k]
-        df_cars = df_cars.append(car_table_from_quote_page(quote_page),
+        df_cars = df_cars.append(car_table_from_summary_page(quote_page),
                                     ignore_index=True)
     df_cars.drop_duplicates(subset= 'id', keep='first', inplace=True)
     df_cars['scraped'] = pd.Timestamp.today()
     return df_cars
 
+def build_detail_link(df_cars):
+    detail_link = 'https://www.autoscout24.de/angebote/'\
+                    +str(re.sub(' ','-',df_cars.headline))\
+                    +str(df_cars.id)+'?cldtidx=1'
+    return detail_link
+
+
+def read_detail_pages(detail_link):
+    quote_page = detail_link
+    response = requests.get(quote_page)
+    soup = BeautifulSoup(response.content, "html.parser")
+    state_str = soup.find("div", {'class':"cldt-categorized-data cldt-data-section sc-pull-left"}).text
+    features_str = soup.find("div", {'class':"cldt-categorized-data cldt-data-section sc-pull-right"}).text
+    return state_str, features_str
+
+
+def scrape_detail_pages(df_cars):
+    state_str = []
+    features_str = []
+    for i in range(0,len(df_cars)):
+        try:
+            time.sleep(1)
+            state_str_i, features_str_i = read_detail_pages(df_cars.detail_link[i])
+            state_str.append(state_str_i)
+            features_str.append(features_str_i)
+        except AttributeError:
+            state_str.append(None)
+            features_str.append(None)
+        except KeyError:
+            state_str.append(None)
+            features_str.append(None)
+    df_cars['state_str'] = state_str
+    df_cars['features_str'] = features_str
+    return df_cars
 
 def main():
     '''
     Applies scrape_data function and saves data frame with time stamp in
     file name in hfd5 format
     '''
-    df_cars = scrape_data()
+    df_cars = scrape_summary_data()
+    df_cars['detail_link'] = df_cars.apply(build_detail_link, axis=1)
+    df_cars = scrape_detail_pages(df_cars)
     df_cars.to_hdf('data/cars_'+str(pd.Timestamp.today().date())+'.h5',
                      key='df_cars',
                      mode='w')
